bebc3a901a868e7e344284c9ad0087fe0312bde5
Matheus Favaro Lemma <matheusflemma123@gmail.com>
2022-01-24 17:00:43 -0300

    feat: project started / initial settings. title component creation / login form styling

---
+++ src/pages/index.js
@@ -0,0 +1,133 @@
+import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import appConfig from "../../config.json";
+
+import Title from "../components/Title";
+
+import GlobalStyle from "../style/GlobalStyle";
+
+export default function Home() {
+	const username = "MatheusFLemma";
+
+	return (
+		<>
+			<GlobalStyle />
+
+			<Box
+				styleSheet={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					backgroundColor: appConfig.theme.colors.primary[500],
+					backgroundImage:
+						"url(https://cdn.wallpapersafari.com/34/55/8ciD7v.png)",
+					backgroundRepeat: "no-repeat",
+					backgroundSize: "cover",
+					backgroundBlendMode: "multiply",
+				}}
+			>
+				<Box
+					styleSheet={{
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "space-between",
+						flexDirection: {
+							xs: "column",
+							sm: "row",
+						},
+						width: "100%",
+						maxWidth: "43.75rem",
+						borderRadius: "5px",
+						padding: "2rem",
+						margin: "1rem",
+						boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+						backgroundColor: appConfig.theme.colors.neutrals[700],
+					}}
+				>
+					<Box
+						as='form'
+						styleSheet={{
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+							justifyContent: "center",
+							width: { xs: "100%", sm: "50%" },
+							textAlign: "center",
+							marginBottom: "2rem",
+						}}
+					>
+						<Title>Boas vindas de volta!</Title>
+
+						<Text
+							variant='body3'
+							styleSheet={{
+								marginBottom: "2rem",
+								color: appConfig.theme.colors.neutrals[300],
+							}}
+						>
+							{appConfig.name}
+						</Text>
+
+						<TextField
+							fullWidth
+							textFieldColors={{
+								neutral: {
+									textColor: appConfig.theme.colors.neutrals[200],
+									mainColor: appConfig.theme.colors.neutrals[900],
+									mainColorHighlight: appConfig.theme.colors.primary[500],
+									backgroundColor: appConfig.theme.colors.neutrals[800],
+								},
+							}}
+						/>
+
+						<Button
+							type='submit'
+							label='Entrar'
+							fullWidth
+							buttonColors={{
+								contrastColor: appConfig.theme.colors.neutrals["000"],
+								mainColor: appConfig.theme.colors.primary[500],
+								mainColorLight: appConfig.theme.colors.primary[400],
+								mainColorStrong: appConfig.theme.colors.primary[600],
+							}}
+						/>
+					</Box>
+
+					<Box
+						styleSheet={{
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+							maxWidth: "12.5rem",
+							padding: "1rem",
+							backgroundColor: appConfig.theme.colors.neutrals[800],
+							border: "1px solid",
+							borderColor: appConfig.theme.colors.neutrals[999],
+							borderRadius: "10px",
+							flex: 1,
+							minHeight: "15rem",
+						}}
+					>
+						<Image
+							styleSheet={{
+								borderRadius: "50%",
+								marginBottom: "1rem",
+							}}
+							src={`https://github.com/${username}.png`}
+						/>
+						<Text
+							variant='body4'
+							styleSheet={{
+								color: appConfig.theme.colors.neutrals[200],
+								backgroundColor: appConfig.theme.colors.neutrals[900],
+								padding: ".1875rem .625rem",
+								borderRadius: "62.5rem",
+							}}
+						>
+							{username}
+						</Text>
+					</Box>
+				</Box>
+			</Box>
+		</>
+	);
+}
